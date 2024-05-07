6b58779f1484e5850e28970a2677cddd7dfaec47
Matías Varela <mativaresc@gmail.com>
2024-05-07 19:17:20 -0300

    Request log

--- src/main/java/me/basedatos2/pencaucu/config/JwtAuthenticationFilter.java
+++ src/main/java/me/basedatos2/pencaucu/config/JwtAuthenticationFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import me.basedatos2.pencaucu.util.JWTUtils;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,6 +16,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 import java.util.ArrayList;
 
+@Log4j2
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
@@ -22,6 +24,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.info("Request: {} : {}", request.getRequestURI(), response.getStatus());
+
         String token = jwtUtils.getTokenFromRequest(request);
         if (token != null && jwtUtils.validateToken(token)) {
             UserDetails userDetails = jwtUtils.getUserDetailsFromToken(token);
--- src/main/java/me/basedatos2/pencaucu/config/SecurityConfig.java
+++ src/main/java/me/basedatos2/pencaucu/config/SecurityConfig.java
@@ -4,11 +4,18 @@ import lombok.RequiredArgsConstructor;
 import me.basedatos2.pencaucu.util.JWTUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -22,6 +29,15 @@ public class SecurityConfig {
         http
                 .csrf(
                         AbstractHttpConfigurer::disable
+                )
+                .cors(cors ->
+                        cors.configurationSource(request -> {
+                            CorsConfiguration corsConfiguration = new CorsConfiguration();
+                            corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:80"));
+                            corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+                            corsConfiguration.setAllowedHeaders(List.of("*"));
+                            return corsConfiguration;
+                        })
                 )
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/auth/**").permitAll()
